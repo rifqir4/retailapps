c4f21b002bae53fa08344bf0c567fbed51f518fe
putriharviana <putriharviana@gmail.com>
2020-10-18 18:23:40 +0700

    add upload image firebase

--- app/components/Firebase.js
+++ app/components/Firebase.js
@@ -1,15 +1,19 @@
 import firebase from "firebase";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCNMPNNTlVJQTL0mNzQUmzxN8Pc20mFDmU",
-    authDomain: "retailapps-f341e.firebaseapp.com",
-    databaseURL: "https://retailapps-f341e.firebaseio.com",
-    projectId: "retailapps-f341e",
-    storageBucket: "retailapps-f341e.appspot.com",
-    messagingSenderId: "584237018934",
-    appId: "1:584237018934:web:6c6ddd9400b6ef79124d97"
-  };
-  // Initialize Firebase
-  const Firebase = firebase.initializeApp(firebaseConfig);
 
-  export default Firebase;
+var firebaseConfig = {
+  apiKey: "AIzaSyAJPBM8K06q40k_0y3ljVEzkxcwK_9UvQ0",
+  authDomain: "reactnativedatabase-39438.firebaseapp.com",
+  databaseURL: "https://reactnativedatabase-39438.firebaseio.com",
+  projectId: "reactnativedatabase-39438",
+  storageBucket: "reactnativedatabase-39438.appspot.com",
+  messagingSenderId: "950339139393",
+  appId: "1:950339139393:web:661e41de967d469a3753c0",
+  // measurementId: "G-X3NBFBH5B3"
+};
+
+// Initialize Firebase
+const Firebase = firebase.initializeApp(firebaseConfig);
+
+export default Firebase;
+
